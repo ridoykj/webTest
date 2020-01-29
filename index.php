a07460b36bbb55d74c8f35069e1770f126370bce
ridoykj <ridoykj@gmail.com>
2020-01-29 23:08:40 +0600

    try again

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Configration</h1>
+    <br>
+    <h4>
+    >> 1st. Move (upload) all file on your server direactory (server: xampp, onlineServer etc.). <br>
+    >> 2nd. Start the Server <br>
+    >> 3rd. execute SQL command (mysql) whice have "README/sqlCommand.txt" Directory <br>
+    >> 4th. if your mysql is not user = 'root' and password = '' please edit api.php file whice have "PHP" Directory and set your password <br>
+    >> 5th. First login username = 'ridoy' and password = 'kumar' <br>
+    >> 6th. You Can Register
+    >> that's it man
+    </h4>
+</body>
+</html>
